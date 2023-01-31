b4fe7c41f7ebf9c72973aa4de8354f9b4a73e9be
Xiao <x.xiao.turquoise@gmail.com>
2023-01-31 23:13:31 +0900

    SSGのところまで

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+
+const name = 'Shin Code';
+export const siteTitle = 'Next.js blog';
+
+function Layout({ children }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico"></link>
+      </Head>
+      <header className={styles.header}>
+        <img src="/images/profile.png" className={utilStyles.borderCircle} />
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+}
+
+export default Layout;
